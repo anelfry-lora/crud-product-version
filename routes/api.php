22845b307714a3f3c5e418b1c91bd90352d33615
Anelfry Lora <anelfrylora@gmail.com>
2023-09-16 11:48:18 -0400

    fix error

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\RegisterUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/register', [RegisterUserController::class, 'store']);
