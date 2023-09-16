ada9ba01ec3018fcca3aa9d576d5e0eb5d0219cb
Anelfry Lora <anelfrylora@gmail.com>
2023-09-16 10:43:46 -0400

    add migration and model of  product and category

---
+++ database/migrations/2023_09_16_141310_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->unsignedBigInteger('category_id');
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
