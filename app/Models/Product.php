0c47556ea49b7fdc4261f882b604ae2c08c2647b
Anelfry Lora <anelfrylora@gmail.com>
2023-09-17 22:31:09 -0400

    Product crud

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'user_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
