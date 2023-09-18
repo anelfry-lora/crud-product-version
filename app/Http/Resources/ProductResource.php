7032115df21bdf936a33f8f664f3a11a890ed442
Anelfry Lora <anelfrylora@gmail.com>
2023-09-17 22:31:28 -0400

    Product crud

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'category_id' => $this->category_id,
+            'user_id' => $this->user_id,
+            'created_at' => (new DateTime($this->created_at))->format('h:i:s a, d/M/Y'),
+        ];
+    }
+}
