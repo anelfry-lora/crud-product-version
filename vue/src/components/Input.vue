18b782a3745e9ae8ec9130fbb2b8ea2d3efb7724
Anelfry Lora <anelfrylora@gmail.com>
2023-09-17 22:31:34 -0400

    Product crud

--- vue/src/components/Input.vue
+++ vue/src/components/Input.vue
@@ -21,7 +21,7 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         ref="input"
-        class="ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 block w-full text-gray-900 border-0 rounded-md shadow-sm"
+        class="bg-gray-50 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 block w-full text-gray-900 border-0 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
     >
