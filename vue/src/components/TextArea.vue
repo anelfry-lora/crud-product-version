6cbe37c2a9c45b3098804605fbcbc679e4997f6f
Anelfry Lora <anelfrylora@gmail.com>
2023-09-17 22:31:38 -0400

    Product crud

---
+++ vue/src/components/TextArea.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+defineProps({
+	modelValue: {
+		type: String,
+		required: true,
+	},
+});
+
+defineEmits(["update:modelValue"]);
+
+const input = ref(null);
+
+onMounted(() => {
+	if (input.value.hasAttribute("autofocus")) {
+		input.value.focus();
+	}
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+<template>
+	<textarea
+        class="bg-gray-50 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 block w-full text-gray-900 border-0 rounded-md shadow-sm"
+        spellcheck="false" rows="4"
+		:value="modelValue"
+		@input="$emit('update:modelValue', $event.target.value)"
+		ref="input">
+    </textarea>
+</template>
